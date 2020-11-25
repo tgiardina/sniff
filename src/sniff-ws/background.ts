e93e0324605abedb172d5b0bfe7f3110315e85b2
Tommy Giardina <tommy.giardina@gmail.com>
2020-11-25 09:38:06 -0600

    Removed @types/chrome dep
    I was using @types/chrome because web-ext-types doesnt support the sender param in addListener callbacks. To remove my dep on @types/chrome, I refactored to use onMessage.sender instead. See: https://github.com/kelseasy/web-ext-types/issues/95

--- src/sniff-ws/background.ts
+++ src/sniff-ws/background.ts
@@ -1,13 +1,12 @@
 import { SniffedMessage } from "../interfaces";
 
 export default (callback: (msg: SniffedMessage) => unknown): void => {
-  chrome.runtime.onConnect.addListener((port) => {
+  browser.runtime.onConnect.addListener((port) => {
     if (port.name === "gdonkey-ws-sniffer") {
-      port.onMessage.addListener((msg, meta) => {
-        meta.sender = <chrome.runtime.MessageSender>meta.sender;
-        if (!meta.sender.tab)
+      port.onMessage.addListener((msg) => {
+        if (!port.sender?.tab)
           throw new Error("Incorrectly set tabs permission");
-        callback(<SniffedMessage>{ tabId: meta.sender.tab.id, ...msg });
+        callback(<SniffedMessage>{ tabId: port.sender.tab.id, ...msg });
       });
     }
   });
